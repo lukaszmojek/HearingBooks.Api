9353a36ed576b49ce6727d6ff20b588f1f3c5f97
lukaszmojek <lukasz.mojek@gmail.com>
2022-05-24 22:26:50 +0200

    Create LiveNotifications service

---
+++ EasySynthesis.LiveNotifications/Program.cs
@@ -0,0 +1,7 @@
+using EasySynthesis.LiveNotifications;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
