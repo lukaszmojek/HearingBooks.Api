626769ecaeabc60d3a1bef509f3033a2dcc8d790
lukaszmojek <lukasz.mojek@gmail.com>
2022-09-29 16:45:48 +0200

    Context C1 diagram created

--- HearingBooks.Structurizr/Program.cs
+++ HearingBooks.Structurizr/Program.cs
@@ -1,19 +1,48 @@
-﻿using Structurizr;
+﻿using HearingBooks.Structurizr;
+using Structurizr;
 using Structurizr.Api;
 
-Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
+Workspace workspace = new Workspace("Hearing Books", "Model of Hearing Books audiobook platform");
 Model model = workspace.Model;
 
-Person user = model.AddPerson("User", "A user of my software system.");
-SoftwareSystem softwareSystem = model.AddSoftwareSystem("Software System", "My software system.");
-user.Uses(softwareSystem, "Uses");
+// Context - C1
+Person user = model.AddPerson(Location.External, "User", "A user of the Hearing Books platform");
+SoftwareSystem cognitiveServices = model.AddSoftwareSystem(Location.External, "Azure Cognitive Services", "Performs synteses with the help of AI");
+SoftwareSystem sendgrid = model.AddSoftwareSystem(Location.External, "SendGrid", "Provider for sending emails");
+
+SoftwareSystem webApp = model.AddSoftwareSystem(Location.Internal, "HearingBooks online", "Allows for creating and browsing syntheses as well as topping up account");
+SoftwareSystem platform = model.AddSoftwareSystem(Location.Internal, "Platform", "Entry point for all requests; gateway to all containers");
+
+user.Uses(webApp, "Uses");
+webApp.Uses(platform, "Fetches data/performs actions requested by the user");
+platform.Uses(cognitiveServices, "Sends requests/downloads synthesis files");
+platform.Uses(sendgrid, "Sends emails using");
+sendgrid.Delivers(user, "Delivers emails to");
 
 ViewSet viewSet = workspace.Views;
-SystemContextView contextView = viewSet.CreateSystemContextView(softwareSystem, "SystemContext", "An example of a System Context diagram.");
+SystemContextView contextView = viewSet.CreateSystemContextView(platform, "HearingBooks context", "Context diagram of HearingBooks platform");
+
 contextView.AddAllSoftwareSystems();
 contextView.AddAllPeople();
+
+// Container - C2
+SoftwareSystem api = model.AddSoftwareSystem(Location.Internal, "Platform", "Entry point for all requests; gateway to all containers");
+
+// Component - C3
 
 Styles styles = viewSet.Configuration.Styles;
 styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
 styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff", Shape = Shape.Person });
 
+StructurizrClient structurizrClient = new StructurizrClient(Secrets.ApiKey, Secrets.ApiSecret);
+structurizrClient.PutWorkspace(76919, workspace);
+
+// Person user = model.AddPerson(Location.External, "User", "A user of the Hearing Books platform");
+//
+// SoftwareSystem webApp = model.AddSoftwareSystem(Location.Internal, "Browser Client", "Web App written in Angular");
+//
+// user.Uses(webApp, "Uses");
+//
+// SoftwareSystem api = model.AddSoftwareSystem(Location.Internal, "Api", "Entry point for all requests; gateway to all containers");
+//
+// webApp.Uses(api, "Fetches data/performs actions requested by the user");
---
+++ HearingBooks.Structurizr/Secrets.cs
@@ -0,0 +1,7 @@
+﻿namespace HearingBooks.Structurizr;
+
+public static class Secrets
+{
+    public static string ApiKey { get; set; }
+    public static string ApiSecret { get; set; }
+}
